a266f156a5efb8134f919e8d1003e1d33ad9af5f
Eric Miller <ericnathanmiller@icloud.com>
2022-09-13 20:14:31 -0400

    Ok one more flag change

--- recipes-devtools/enviro-test/files/start_logging.sh
+++ recipes-devtools/enviro-test/files/start_logging.sh
@@ -3,7 +3,7 @@
 sync_time.bash
 set_db_flag.sh "gpu=0"
 set_db_flag.sh "stress-ng=0"
-set_db_flag.sh "memtester=0"
+set_db_flag.sh "mem=0"
 set_db_flag.sh "rt=0"
 python3 /usr/bin/tegra-pwrmon.py &
 /usr/bin/tegrastats_influx.sh &
